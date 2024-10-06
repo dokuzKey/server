dd7a8970ccb94a6e4862c4bb9d5bdc7489784dec
Mustafa Yıldız <musti@Mustafa-MacBook-Air.local>
2024-10-06 15:34:21 +0300

    fix: consistency

--- src/modules/auth/login.ts
+++ src/modules/auth/login.ts
@@ -17,20 +17,20 @@ const login = async (req: Request, res: Response): Promise<void> => {
     };
     const user = await User.findOne({ username });
     if (!user) {
-      res.status(404).json({ code: 0, message: 'User not found' });
+      res.status(404).json({ status: 0, message: 'User not found' });
       return;
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      res.status(401).json({ code: 0, message: 'Invalid password' });
+      res.status(401).json({ status: 0, message: 'Invalid password' });
       return;
     }
     res
       .status(200)
-      .json({ code: 1, message: 'Login successful', token: user.token });
+      .json({ status: 1, message: 'Login successful', token: user.token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ code: 0, message: 'Internal server error' });
+    res.status(500).json({ status: 0, message: 'Internal server error' });
   }
 };
 
